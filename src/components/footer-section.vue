378b765f6b327fbea8590cb711e51c72a0bb9cf9
svalvastic <assogames2@gmail.com>
2022-09-22 18:36:54 +0200

    ✨ feat(footer): add footer

--- src/components/footer-section.vue
+++ src/components/footer-section.vue
@@ -1,15 +1,106 @@
 <template>
-  <div>
+  <section>
+
   
-  </div>
+    <footer class="upper-footer">
+      <div class="container">
+        <div class="link-section">
+            <div class="col">
+            <link-list class="link-list" />
+          <link-list class="link-list" />
+            </div>
+          
+          <link-list class="link-list" />
+          <link-list class="link-list" />
+        </div>
+
+        <div class="footer-jumbo"></div>
+      </div>
+    </footer>
+
+
+    <footer class="down-footer">
+      <button>Sign up now!</button>
+      <div class="social">
+        <p>FOLLOW US</p>
+        <a href="#">F</a>
+        <a href="#">F</a>
+        <a href="#">F</a>
+        <a href="#">F</a>
+        <a href="#">F</a>
+      </div>
+    </footer>
+  </section>
 </template>
 
 <script>
+import linkList from "./link-list.vue";
 export default {
-
-}
+  components: { linkList },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../variable.sass";
 
+section {
+  background-color: $footer-color;
+
+  z-index: 10;
+
+  .upper-footer {
+    position: relative;
+    background-image: url("../assets/img/footer-bg.jpg");
+    background-size: cover;
+    height: 400px;
+
+    .container {
+        height: 100%;
+        @include container;
+        @include flex-center;
+      .link-section {
+        display: flex;
+        flex-direction: row;
+
+      }
+      .footer-jumbo {
+        align-self: flex-end;
+        background-image: url("../assets/img/dc-logo-bg.png");
+        background-repeat: no-repeat;
+        background-color: transparent;
+        background-position-y: -50px;
+        width: 50%;
+        height: 100%;
+      }
+    }
+  }
+
+  .down-footer {
+    background-color: $footer-color;
+    padding: 2rem;
+    @include container;
+    @include flex-center;
+  }
+}
+
+
+.social {
+  @include flex-center;
+  font-weight: bold;
+  color: $brand-color;
+
+  a {
+    padding: 1.5rem;
+    color: white;
+    text-decoration: none;
+  }
+}
+
+button {
+  padding: 0.8rem 1.5rem;
+  font-size: 1.5rem;
+  background-color: transparent;
+  color: white;
+  border: 2px solid $brand-color;
+}
 </style>
---
+++ src/components/link-list.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+  <ul>
+
+  <li class="title">ciao</li>
+  <li>ciao</li>
+  <li>ciao</li>
+  <li>ciao</li>
+  <li>ciao</li>
+  <li>ciao</li>
+
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../variable.sass";
+div {
+    margin: 1rem
+}
+
+.title {
+    font-size: 1.5rem;
+    font-weight: 700;
+    text-transform: uppercase;
+    color:$background;
+    margin-bottom: 0.5rem
+}
+ul {
+    list-style: none;
+}
+li {
+    color: rgba(255, 255, 255, 0.356);
+}
+
+</style>
